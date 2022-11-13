ab16e2985f738703c675373bd04f57e8dbaaf5dc
Liquid Monks <info@liquidmonks.com>
2022-11-12 18:50:52 -0600

    script.js includes additional global variable sets that grabs element IDs from index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,20 @@ let quizBoardID = document.getElementById("quiz-board");
 // Quiz Over Section
 let quizOverID = document.getElementById("quiz-over");
 let scoreAnnounceID = document.getElementById("score-announce");
+let recordInitialsID = document.getElementById("record-initials"); // Input form section
+
+// Scoreboard Section
+let scoreboardGridID = document.getElementById("scoreboard-grid");
+let winnersListID = document.getElementById("winners-list");
+
+// Header Section
+let scoreboardID = document.getElementById("scoreboard");
+
+// Quiz Key Section
+let correctID = document.getElementById("correct");
+let incorrectID = document.getElementById("incorrect");
+
+// Quiz Button Elements
+let btnStartGameID = document.getElementById("start-game"); // Quiz info and description section
+let btnReturnID = document.getElementById("return"); // Scoreboard grid section
+let btnClearScoreboardID = document.getElementById("clear-scoreboard"); // Scoreboard grid section
