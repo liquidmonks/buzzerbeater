5317fbe7a54e51a535f366d232fa9490ad850204
Liquid Monks <info@liquidmonks.com>
2022-11-12 18:16:57 -0600

    script.js includes global variable sets that grabs element IDs from index.html

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// This code quiz shallow allow the user to start the quiz, answer questions, show the user if the question is correct or incorrect, then allow the user to enter their initials to the scoreboard
+
+/* Global Variable Sets */
+
+// Quiz Board Section
+let quizQuestionsID = document.getElementById("quiz-questions");
+let quizBoardID = document.getElementById("quiz-board");
+
+// Quiz Over Section
+let quizOverID = document.getElementById("quiz-over");
+let scoreAnnounceID = document.getElementById("score-announce");
